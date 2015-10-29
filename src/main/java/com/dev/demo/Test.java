564240a4dadc4cf51d91993689e9d9f7b253ae0c
Fichtali Mh <fichtali.mehdi@hotmail.fr>
2015-10-29 10:49:13 +0000

    commit ws by MF

---
+++ src/main/java/com/dev/demo/Test.java
@@ -0,0 +1,28 @@
+package com.dev.demo;
+
+public class Test {
+
+	private String nom;
+	private String prenon;
+
+	public Test() {
+		super();
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenon() {
+		return prenon;
+	}
+
+	public void setPrenon(String prenon) {
+		this.prenon = prenon;
+	}
+
+}
---
+++ src/main/java/com/dev/demo/WelcomeRestController.java
@@ -0,0 +1,21 @@
+package com.dev.demo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+public class WelcomeRestController {
+	
+	
+	@RequestMapping(value = "/{username}", method = RequestMethod.GET, headers = "Accept=application/json")
+	public Test tellWelcome(@PathVariable String username){
+		Test t = new Test();
+		t.setNom("Erraji");
+		t.setPrenon(username);
+		return t;
+	}
+
+}
